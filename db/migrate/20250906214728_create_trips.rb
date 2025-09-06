60ba28f3040862c2650f99fb46d443d51c7b85ae
Usborn Ocampo <usbornocampo@Usborns-MacBook-Air.local>
2025-09-06 14:48:06 -0700

    init commit and trip scaffold

---
+++ db/migrate/20250906214728_create_trips.rb
@@ -0,0 +1,12 @@
+class CreateTrips < ActiveRecord::Migration[8.0]
+  def change
+    create_table :trips do |t|
+      t.string :name
+      t.string :location
+      t.date :start_date
+      t.date :end_date
+
+      t.timestamps
+    end
+  end
+end
