1c083bba635915f8aec86347c30469536a0f3285
usborn116 <usborn116@gmail.com>
2025-10-30 15:03:20 -0700

    add basic fixtures and tests for summing transactions in a certain budget category

--- app/models/budget_category.rb
+++ app/models/budget_category.rb
@@ -1,5 +1,9 @@
 class BudgetCategory < ApplicationRecord
   belongs_to :trip
   has_many :transactions
+  
+  def sum_transactions
+    transactions.sum(:amount)
+  end
 end
 
--- spec/models/budget_category_spec.rb
+++ spec/models/budget_category_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe BudgetCategory, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#sum_transactions' do
+    it 'returns the sum of all transactions for the food category' do
+      budget_category = budget_categories(:food)
+
+      expect(budget_category.sum_transactions).to eq(15.00)
+    end
+    
+    it 'returns the sum of all transactions for the shopping category' do
+      budget_category = budget_categories(:shopping)
+
+      expect(budget_category.sum_transactions).to eq(129.99)
+    end
+  end
 end
