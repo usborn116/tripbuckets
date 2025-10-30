951e3da01776ec8e78e8a10d4dbd9aba1e41f14e
usborn116 <usborn116@gmail.com>
2025-10-30 15:16:31 -0700

    change public API for total spent in a category

--- app/models/budget_category.rb
+++ app/models/budget_category.rb
@@ -2,8 +2,16 @@ class BudgetCategory < ApplicationRecord
   belongs_to :trip
   has_many :transactions
   
+  def spent
+    update(sum_cache: sum_transactions) if sum_cache.nil?
+    sum_cache
+  end
+  
+  private
+  
   def sum_transactions
     transactions.sum(:amount)
   end
+  
 end
 
--- spec/models/budget_category_spec.rb
+++ spec/models/budget_category_spec.rb
@@ -1,17 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe BudgetCategory, type: :model do
-  describe '#sum_transactions' do
+  describe '#spent' do
     it 'returns the sum of all transactions for the food category' do
       budget_category = budget_categories(:food)
 
-      expect(budget_category.sum_transactions).to eq(15.00)
+      expect(budget_category.spent).to eq(15.00)
     end
     
     it 'returns the sum of all transactions for the shopping category' do
       budget_category = budget_categories(:shopping)
 
-      expect(budget_category.sum_transactions).to eq(129.99)
+      expect(budget_category.spent).to eq(129.99)
     end
   end
 end
