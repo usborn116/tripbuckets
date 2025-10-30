155ec9ac2290e20025d513c9f2371f385d90b0fd
usborn116 <usborn116@gmail.com>
2025-10-30 15:59:45 -0700

    add hooks to update a transaction's parent category's sum cache when transaction is saved or deleted

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,4 +1,16 @@
 class Transaction < ApplicationRecord
-  belongs_to :trip
   belongs_to :budget_category
+
+  after_save :increment_budget_spent
+  after_destroy :decrement_budget_spent
+
+  private
+
+  def increment_budget_spent
+    budget_category.add(amount) 
+  end
+  
+  def decrement_budget_spent
+    budget_category.subtract(amount)
+  end
 end
--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'hooks' do
+    it 'updates budget category sum cache after save' do
+      budget_category = budget_categories(:food)
+      previous_spent = budget_category.spent
+      transaction = budget_category.transactions.create!(amount: 10.00)
+      expect(budget_category.reload.spent).to eq(previous_spent + 10.00)
+    end
+
+    it 'removes from budget category sum cache after destroy' do
+      budget_category = budget_categories(:food)
+      previous_spent = budget_category.spent
+      transaction = transactions(:burger)
+      transaction.destroy!
+      expect(budget_category.reload.spent).to eq(previous_spent - 10.01)
+    end
+  end
 end
